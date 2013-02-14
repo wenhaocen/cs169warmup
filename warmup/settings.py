44c3b17865c14efe006aa17ed3a3239102daacdb
Wenhao Cen <cenwenhao@hotmail.com>
2013-02-14 00:18:42 -0800

    final

--- warmup/settings.py
+++ warmup/settings.py
@@ -7,16 +7,9 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config()
 }
 
 # Local time zone for this installation. Choices can be found here:
